7726c0f74c7fdd83170ed4ecbdf0b30521bef3ce
Yonibelay233 <belayeyonatan@gmail.com>
2026-09-18 13:03:06 +0300

    Adding the data source and starting the ViewModel.

---
+++ ViewModel/CompanysDB.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using System.Data.OleDb;
+
+namespace ViewModel
+{
+    public class CompanysDB : BaseDB
+    {
+        public CompanysList SelectAll()
+        {
+            command.CommandText = $"SELECT * FROM Companys";
+            CompanysList companysLst = new CompanysList(base.Select());
+            return companysLst;
+        }
+
+        public override BaseEntity NewEntity()
+        {
+            return new Companys();
+        }
+
+        protected override BaseEntity CreateModel(BaseEntity entity)
+
+        {
+            Companys c = entity as Companys;
+            c.CompanyName = reader["CompanyName"].ToString();
+            base.CreateModel(entity);
+            return c;
+        }
+    }
+}
---
+++ ViewModel/RoleDB.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using System.Data.OleDb;
+
+namespace ViewModel
+{
+    public class RoleDB : BaseDB
+    {
+        public RoleList SelectAll()
+        {
+            command.CommandText = $"SELECT * FROM Role";
+            RoleList roleLst = new RoleList(base.Select());
+            return roleLst;
+        }
+
+        public override BaseEntity NewEntity()
+        {
+            return new Role();
+        }
+
+        protected override BaseEntity CreateModel(BaseEntity entity)
+
+        {
+            Role r = entity as Role;
+            r.RoleName = reader["RoleName"].ToString();
+            base.CreateModel(entity);
+            return r;
+        }
+
+    }
+}
